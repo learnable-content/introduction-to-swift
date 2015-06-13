d7b05475bc8917d0f0d0f95b01139911b4f2af6e
bitesizebschool <brettromero@MacBook-Pro.local>
2015-06-12 22:19:52 -0700

    start code + readme for 2.3 start

---
+++ ActivityLogger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ActivityLogger
+//
+//  Created by Brett Romero on 6/8/15.
+//  Copyright (c) 2015 Brett Romero. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # introduction-to-swift
+Lesson 2.3
+
+* Integrating third scene
+* Add tab item from object library to third scene
+* Change scene name to "Add"
+* Create segue from Tab Controller to Add scene
+  This creates a relationship between these two UI elements
+  We'll now have a third tab on the Tab Bar Controller
+* Explanation of segues
+* Run app to see newly linked third scene
+* Explanation of Tab Controller's role in app
+* Recap of the relationships between scenes
+* Explanation of storyboard
