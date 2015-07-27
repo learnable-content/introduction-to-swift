04944a173de807642d21eb83a4cb4ead618eb940
bitesizebschool <brettromero@MacBook-Pro.local>
2015-07-26 17:10:51 -0700

    initial check for 4.3 start

---
+++ ActivityLogger/AddViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddViewController.swift
+//  ActivityLogger
+//
+//  Created by Brett Romero on 7/22/15.
+//  Copyright (c) 2015 Brett Romero. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var activityLabel: UITextField!
+    
+    @IBAction func addActivity_Click(sender: AnyObject) {
+        ActivityManager.activities.append(Activity(activityName: activityLabel.text, totalTime: 0))
+        activityLabel.text = ""
+        activityLabel.resignFirstResponder()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-Lesson 4.2 View Activity Totals
+4.3 Activity Management Scene
 
-* Modify the assignment of activity name and time to tableview cells
-* Explanation string concatenation syntax being used
-
-Note to reviewer: This lesson will be shorter than I anticipated.  Much of the model discussion mentioned in the lesson description has already been covered
+* Add new UIViewController for Add Scene
+* Connect Add ViewController in storyboard with AddViewController class
+* Create IBOutlet for text field
+* Create IBAction for "Save Activity" button
+* Discuss code used in IBAction
+* Create IBOutlet in Performance view for the tableview
+* Discuss reloading Performance tableview
+* Run app to demo
